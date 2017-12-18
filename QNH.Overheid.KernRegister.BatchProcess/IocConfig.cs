2f7eddc853576bacb9081e34ad49f634f2f7422d
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2017-12-18 12:38:32 +0100

    Removed unused RSGB repository

--- QNH.Overheid.KernRegister.BatchProcess/IocConfig.cs
+++ QNH.Overheid.KernRegister.BatchProcess/IocConfig.cs
@@ -172,15 +172,6 @@ namespace QNH.Overheid.KernRegister.BatchProcess
                         x.For(typeof(IRepository<>)).Use(typeof(NHRepository<>)).AlwaysUnique();
                         x.For<IKvkInschrijvingRepository>().Use<KvkInschrijvingNHRepository>().AlwaysUnique();
 
-                        if (SettingsHelper.BrmoApplicationEnabled)
-                        {
-                            x.For(typeof(IRsgbRepository<>)).Use(typeof(RsgbRepository<>))
-                                .Ctor<ISession>()
-                                .Is((ctx) => ctx.GetInstance<ISessionFactory>()
-                                                .OpenSession(new OracleConnection(
-                                                    ConfigurationManager.ConnectionStrings["OracleRsgbConnection"].ConnectionString)));
-                        }
-
                         // End nHibernate setup
                         break;
                     default:
