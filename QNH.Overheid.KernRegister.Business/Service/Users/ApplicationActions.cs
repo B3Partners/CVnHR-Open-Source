dc8a65e7569808e2f04244095065849373476899
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2018-01-05 18:15:12 +0100

    Added action descriptions

--- QNH.Overheid.KernRegister.Business/Service/Users/ApplicationActions.cs
+++ QNH.Overheid.KernRegister.Business/Service/Users/ApplicationActions.cs
@@ -16,4 +16,20 @@ namespace QNH.Overheid.KernRegister.Business.Service.Users
         Crediteuren,
         Admin
     }
+
+    public static class ApplicationActionDescriptions
+    {
+        private static Dictionary<ApplicationActions, string> Descriptions => new Dictionary<ApplicationActions, string>
+        {
+            { ApplicationActions.Admin, "Administrator functie (Gebruikersbeheer)" },
+            { ApplicationActions.Brmo, "Gebruiker mag doorzetten naar BRMO" },
+            { ApplicationActions.Crediteuren, "Gebruiker mag doorzetten naar Crediteuren" },
+            { ApplicationActions.Debiteuren, "Gebruiker mag doorzetten naar Debiteuren" },
+            { ApplicationActions.Crm, "Gebruiker mag doorzetten naar Crm" },
+            { ApplicationActions.ManageKvKData, "Gebruiker mag synchroniseren met KvK " },
+            { ApplicationActions.ViewKvKData, "Gebruiker mag kvk gegevens zien" }
+        };
+
+        public static string Get(ApplicationActions action) => Descriptions[action];
+    }
 }
