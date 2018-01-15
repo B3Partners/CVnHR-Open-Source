7cee4e1bced37424b36b884da7ac02c2e7dfa90d
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2018-01-15 15:38:07 +0100

    - Basic refactoring of filters - Added main authorization rule

---
+++ QNH.Overheid.KernRegister.Beheer/Filters/CVnHRAuthorizeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using QNH.Overheid.KernRegister.Beheer.Utilities;
+using QNH.Overheid.KernRegister.Business.Service.Users;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+
+namespace QNH.Overheid.KernRegister.Beheer.Controllers
+{
+    public class CVnHRAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        private ApplicationActions[] _actions;
+        private static readonly ApplicationActions[] _allActions 
+            = Enum.GetValues(typeof(ApplicationActions)).OfType<ApplicationActions>().ToArray();
+
+        public CVnHRAuthorizeAttribute(params ApplicationActions[] actions)
+        {
+            _actions = actions;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var user = httpContext.User;
+            return _actions.Any()
+                ? user.IsAllowedAllActions(_actions)
+                : user.IsAllowedAnyActions(_allActions);
+        }
+    }
+}
