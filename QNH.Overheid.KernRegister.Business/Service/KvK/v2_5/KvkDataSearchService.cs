d211e3acf53ca90d8707ff1e08449abf07c0395b
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2018-04-16 22:15:41 +0200

    Add Polly and retry on kvkErrors

--- QNH.Overheid.KernRegister.Business/Service/KvK/v2_5/KvkDataSearchService.cs
+++ QNH.Overheid.KernRegister.Business/Service/KvK/v2_5/KvkDataSearchService.cs
@@ -10,6 +10,7 @@ using System.Xml;
 using NLog;
 using QNH.Overheid.KernRegister.Business.Business;
 using QNH.Overheid.KernRegister.Business.KvK;
+using QNH.Overheid.KernRegister.Business.KvK.Exceptions;
 using QNH.Overheid.KernRegister.Business.KvK.v2._5;
 using QNH.Overheid.KernRegister.Business.Model.Entities;
 
@@ -95,7 +96,7 @@ namespace QNH.Overheid.KernRegister.Business.Service.KvK.v2_5
                 // Log the specific kvk Error
                 KvkErrorLogger.Error(kvkNummer + " | " + string.Join("; ", errors.Select(e => e.MessageType + ": " + e.Message)));
 
-                throw new Exception(msg);
+                throw new KvkServerException(msg);
             }
 
             // Add the current requested inschrijving to cache
