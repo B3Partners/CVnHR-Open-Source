ed365046f8b298ba64756d1fec2d69a648ac8682
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2018-07-04 21:25:05 +0200

    - Added simple semaphore to only allow one thread per KvKnummer to update. (In same process...) - Added project to reproduce error, and test current solution

--- QNH.Overheid.KernRegister.Business/Service/InschrijvingSyncService.cs
+++ QNH.Overheid.KernRegister.Business/Service/InschrijvingSyncService.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using QNH.Overheid.KernRegister.Business.Enums;
+﻿using QNH.Overheid.KernRegister.Business.Enums;
 using QNH.Overheid.KernRegister.Business.Model;
 using QNH.Overheid.KernRegister.Business.Model.Entities;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
 
 namespace QNH.Overheid.KernRegister.Business.Service
 {
     public class InschrijvingSyncService : IInschrijvingSyncService, IDisposable
     {
         private readonly IKvkInschrijvingRepository _kvkInschrijvingRepo;
+        private static readonly ConcurrentBag<string> CurrentKvkNummersActive = new ConcurrentBag<string>();
 
         public InschrijvingSyncService(IKvkInschrijvingRepository kvkInschrijvingRepo)
         {
@@ -23,54 +25,68 @@ namespace QNH.Overheid.KernRegister.Business.Service
         /// <param name="kvkInschrijving"></param>
         public AddInschrijvingResultStatus AddNewInschrijvingIfDataChanged(KvkInschrijving kvkInschrijving)
         {
-            // Ensure unique sbiCode objects
-            foreach (var activiteit in kvkInschrijving.SbiActiviteiten)
+            var kvkNummer = kvkInschrijving.KvkNummer;
+            try
             {
-                var code = activiteit.SbiCode;
-                activiteit.SbiCode = _kvkInschrijvingRepo.GetSbiCode(code.Code, code.Omschrijving);
-            }
-            // Ensure unique sbiCodes objects per vestiging
-            foreach (var vestiging in kvkInschrijving.Vestigingen)
-                foreach (var activiteit in vestiging.SbiActiviteiten)
+                while (CurrentKvkNummersActive.Contains(kvkNummer))
+                {
+                    Thread.Sleep(50);
+                }
+                CurrentKvkNummersActive.Add(kvkNummer);
+
+                // Ensure unique sbiCode objects
+                foreach (var activiteit in kvkInschrijving.SbiActiviteiten)
                 {
                     var code = activiteit.SbiCode;
                     activiteit.SbiCode = _kvkInschrijvingRepo.GetSbiCode(code.Code, code.Omschrijving);
                 }
+                // Ensure unique sbiCodes objects per vestiging
+                foreach (var vestiging in kvkInschrijving.Vestigingen)
+                    foreach (var activiteit in vestiging.SbiActiviteiten)
+                    {
+                        var code = activiteit.SbiCode;
+                        activiteit.SbiCode = _kvkInschrijvingRepo.GetSbiCode(code.Code, code.Omschrijving);
+                    }
 
-            // Now get the latest Inschrijving from the KVK register database
-            var latestInschrijvingFromDb = _kvkInschrijvingRepo.GetLatestInschrijving(kvkInschrijving.KvkNummer);
-            if (latestInschrijvingFromDb == null) // Just store the new one
-            {
+                // Now get the latest Inschrijving from the KVK register database
+                var latestInschrijvingFromDb = _kvkInschrijvingRepo.GetLatestInschrijving(kvkInschrijving.KvkNummer);
+                if (latestInschrijvingFromDb == null) // Just store the new one
+                {
+                    kvkInschrijving.IngevoegdOp = DateTime.Now.AddSeconds(1);
+                    kvkInschrijving.GeldigTot = DateTime.MaxValue;
+                    _kvkInschrijvingRepo.AddOrUpdate(kvkInschrijving);
+                    return AddInschrijvingResultStatus.NewInschrijvingAdded;
+                }
+
+                // If one is found, let's compare it with the one that is supplied
+                if (latestInschrijvingFromDb.FunctionalEquals(kvkInschrijving))
+                {
+                    // if they are the same, do nothing
+                    return AddInschrijvingResultStatus.InschrijvingAlreadyExists;
+                }
+
+                // Set the expiration date for the old inschrijving including all its vestigingen
+                latestInschrijvingFromDb.GeldigTot = DateTime.Now;
+                foreach (var vestiging in latestInschrijvingFromDb.Vestigingen)
+                    vestiging.GeldigTot = DateTime.Now;
+                _kvkInschrijvingRepo.AddOrUpdate(latestInschrijvingFromDb);
+
+                // Create the new inchrijving
                 kvkInschrijving.IngevoegdOp = DateTime.Now.AddSeconds(1);
                 kvkInschrijving.GeldigTot = DateTime.MaxValue;
+                foreach (var vestiging in kvkInschrijving.Vestigingen)
+                {
+                    vestiging.IngevoegdOp = DateTime.Now.AddSeconds(1);
+                    vestiging.GeldigTot = DateTime.MaxValue;
+                }
                 _kvkInschrijvingRepo.AddOrUpdate(kvkInschrijving);
-                return AddInschrijvingResultStatus.NewInschrijvingAdded;
-            }
 
-            // If one is found, let's compare it with the one that is supplied
-            if (latestInschrijvingFromDb.FunctionalEquals(kvkInschrijving))
+                return AddInschrijvingResultStatus.InschrijvingUpdated;
+            }
+            finally
             {
-                // if they are the same, do nothing
-                return AddInschrijvingResultStatus.InschrijvingAlreadyExists;
+                CurrentKvkNummersActive.TryTake(out kvkNummer);
             }
-            
-            // Set the expiration date for the old inschrijving including all its vestigingen
-            latestInschrijvingFromDb.GeldigTot = DateTime.Now;
-            foreach (var vestiging in latestInschrijvingFromDb.Vestigingen)
-                vestiging.GeldigTot = DateTime.Now;
-            _kvkInschrijvingRepo.AddOrUpdate(latestInschrijvingFromDb);
-
-            // Create the new inchrijving
-            kvkInschrijving.IngevoegdOp = DateTime.Now.AddSeconds(1);
-            kvkInschrijving.GeldigTot = DateTime.MaxValue;
-            foreach (var vestiging in kvkInschrijving.Vestigingen)
-            {
-                vestiging.IngevoegdOp = DateTime.Now.AddSeconds(1);
-                vestiging.GeldigTot = DateTime.MaxValue;
-            }
-            _kvkInschrijvingRepo.AddOrUpdate(kvkInschrijving);
-
-            return AddInschrijvingResultStatus.InschrijvingUpdated;
         }
 
         public void Dispose()
