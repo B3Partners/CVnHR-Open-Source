27e7b373655368df4ec4ccd74aeb2684207d6948
mvdstruijk <m_struijk1990@hotmail.nl>
2018-11-29 10:13:02 +0100

    added the the controller for the task manager view

---
+++ QNH.Overheid.KernRegister.Beheer/Controllers/TasksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using QNH.Overheid.KernRegister.Business.Service.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QNH.Overheid.KernRegister.Beheer.Controllers
+{
+    [CVnHRAuthorize(ApplicationActions.CVnHR_Tasks)]
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View(this);
+        }
+
+
+        public IEnumerable<Task> TaskProcesses
+        {
+            get
+            {
+                TaskService ts = new TaskService();
+                return ts.RootFolder.GetTasks(new System.Text.RegularExpressions.Regex(@"CVnHR")).ToArray();
+            }
+        }
+    }
+}
