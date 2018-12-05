9c230d35ba683f2df286648b0473449f7b0e5402
mvdstruijk <m_struijk1990@hotmail.nl>
2018-12-05 13:51:56 +0100

    Fixes for IE

--- QNH.Overheid.KernRegister.Beheer/Scripts/FileUpload/brmo.js
+++ QNH.Overheid.KernRegister.Beheer/Scripts/FileUpload/brmo.js
@@ -43,7 +43,7 @@ $(function () {
 
 
 $("document").ready(function () {
-    var name = new URL(window.location.href).searchParams.get("name");
+    var name = getParameterByName("name");
     $("#Task-name").val(name.substr(name.indexOf(' ') + 1));
 
     $("#processType").on('change', function () {
@@ -61,12 +61,22 @@ $("document").ready(function () {
         var names = $("#Brmo-PostCodes").val().split(" ");
         for (var i = 0; i < rows.length; i++) {
             for (var j = 0; j < names.length; j++) {
-                if (names[j] == rows[i].children[1].innerText.replace(/(\r\n|\n|\r)/gm, "")) {
+                if (names[j] == rows[i].children[1].innerText.replace(/(\r\n|\n|\r)/gm, "").trim()) {
                     $(rows[i].children[3]).find("input").prop("checked", true);
                 }
             }
         }
         clearInterval(inter);
+    };
+
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
     };
 
     if ($("#processType").val() === "Csv") {
@@ -88,7 +98,7 @@ $("document").ready(function () {
             var csvCount = 0;
             for (var i = 0; i < rows.length; i++) {
                 if ($(rows[i].children[3]).find("input").is(":checked")) {
-                    PostCodes += rows[i].children[1].innerText;
+                    PostCodes += rows[i].children[1].innerText.trim();
                     csvCount++;
                 }
             }
@@ -106,7 +116,7 @@ $("document").ready(function () {
         }
         $.post(window.location.href.split('?')[0] + "/SaveConfig", { PostCodes: PostCodes, taskName: taskName, HRDataserviceVersion: HRDataserviceVersion, BrmoProcessType: BrmoProcessType })
             .success(function () {
-                location.replace(window.location.origin + "/Brmo?name=CVnHR " + taskName);
+                location.replace(window.location.origin + window.location.pathname + "?name=CVnHR " + taskName);
                 alert("Taak is opgeslagen");
             })
             .fail(function () {
