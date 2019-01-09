c45d98bfc754ca4121b6e1c75d6127cb0a929624
Corné Hogerheijde <corne.hogerheijde@qnh.nl>
2019-01-09 16:38:44 +0100

    Updated bootstrap to bootstrap4 => plus migrations

--- QNH.Overheid.KernRegister.Beheer/App_Start/BootstrapBundleConfig.cs
+++ QNH.Overheid.KernRegister.Beheer/App_Start/BootstrapBundleConfig.cs
@@ -14,7 +14,9 @@ namespace QNH.Overheid.KernRegister.Beheer.App_Start
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap-theme").Include("~/Content/bootstrap-theme.css"));
+			BundleTable.Bundles.Add(new StyleBundle("~/Content/bootstrap-theme")
+                .Include("~/Content/bootstrap-grid.css")
+                .Include("~/Content/bootstrap-reboot.css"));
 		}
 	}
 }
