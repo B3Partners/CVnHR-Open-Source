1ae5bda080564217b56f6f34fc647e714d359e08
Corné Hogerheijde <corne@hogerheijde.nl>
2019-03-27 21:41:13 +0100

    Updated nugets

--- QNH.Overheid.KernRegister.Business/Utility/NHibernateUtils.cs
+++ QNH.Overheid.KernRegister.Business/Utility/NHibernateUtils.cs
@@ -2,22 +2,18 @@
 using NHibernate.Engine;
 using NHibernate.Hql.Ast.ANTLR;
 using NHibernate.Linq;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace QNH.Overheid.KernRegister.Business.Utility
 {
     public static class NHibernateUtils
     {
-        public static String GetGeneratedSql(this IQueryable queryable, ISession session)
+        public static string GetGeneratedSql(this IQueryable queryable, ISession session)
         {
             var sessionImp = (ISessionImplementor)session;
             var nhLinqExpression = new NhLinqExpression(queryable.Expression, sessionImp.Factory);
             var translatorFactory = new ASTQueryTranslatorFactory();
-            var translators = translatorFactory.CreateQueryTranslators(nhLinqExpression.Key, nhLinqExpression.Key, false, sessionImp.EnabledFilters, sessionImp.Factory);
-                //translatorFactory.CreateQueryTranslators(nhLinqExpression.Key, nhLinqExpression, null, false, sessionImp.EnabledFilters, sessionImp.Factory);
+            var translators = translatorFactory.CreateQueryTranslators(nhLinqExpression, nhLinqExpression.Key, false, sessionImp.EnabledFilters, sessionImp.Factory);
 
             return translators[0].SQLString;
         }
