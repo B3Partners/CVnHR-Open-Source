aa0823e2276e0485b742b515800dcabeae46405f
Corné Hogerheijde <corne@hogerheijde.nl>
2019-06-19 12:59:36 +0200

    Removed local configs from build action

--- QNH.Overheid.KernRegister.Beheer/Scripts/FileUpload/mutaties.js
+++ QNH.Overheid.KernRegister.Beheer/Scripts/FileUpload/mutaties.js
@@ -2,6 +2,8 @@
 
 $("document").ready(function () {
 
+    var processStarted = false;
+
     // Save postcodes
     $("#MutationSaveZipcodes").on('click', function () {
         var zipCodes = $("#MutationZipCodes").val();
@@ -16,13 +18,40 @@ $("document").ready(function () {
     });
 
     $("#DownloadAndProcess").on('click', function () {
+        setDisabled(true);
         var url = $(this).attr('data-url');
         $.get(url)
             .done(function (result) {
-                console.log(result);
+                setDisabled(false);
                 StartProcess(result);
             });
     });
+
+    $("#Download").on('click', function () {
+        setDisabled(true);
+        var url = $(this).attr('data-url');
+        $.get(url)
+            .done(function (result, ok, xhr) {
+                setDisabled(false);
+
+                // create csv to download
+                var csvContent = "data:text/csv;charset=utf-8," + result;
+                var fileName = xhr.getResponseHeader("Content-Disposition")
+                    .replace('attachment; filename=', '');
+
+                var encodedUri = encodeURI(csvContent);
+                var link = document.createElement("a");
+                link.setAttribute("href", encodedUri);
+                link.setAttribute("download", fileName);
+                document.body.appendChild(link); // Required for FF
+
+                link.click(); // This will download the data file named fileName.
+            });
+    });
+
+    function setDisabled(disabled) {
+        $("select, input, textarea, button").attr('disabled', disabled ? 'disabled' : null);
+    }
 
     // Mutation task actions
     var csvMutatieHub = $.connection.csvMutatieHub;
@@ -63,6 +92,7 @@ $("document").ready(function () {
             }
 
             $.connection.hub.stop();
+            processStarted = false;
         }
     };
 
@@ -72,13 +102,13 @@ $("document").ready(function () {
             // (Refer to the client side documentatio).
         })
         .on('fileuploaddone', function (e, data) {
-
-            var fileName = data.files[0].name;
-            StartProcess(fileName);
+            StartProcess(data.files[0].name);
         });
 
     function StartProcess(fileName) {
         $('#mutatiedialog').on('shown.bs.modal', function (e) {
+            if (processStarted) return;
+            processStarted = true;
             // do something...
             $.connection.hub.start().done(function () {
                 var brmoChecked = $('#brmo-mutations').prop('checked');
